ad5a88c376feea4159a55d2ca8259392061e856c
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2021-01-25 14:06:40 +0530

    Add files via upload

---
+++ nandincpp/Search Insert Position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++)
+        {
+            if(target<=nums[i])
+            {
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
