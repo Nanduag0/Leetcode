8de9ebf5bd3d33c87ab9fe22ccd26da3e28c4df7
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2021-03-11 13:16:56 +0530

    Add files via upload

---
+++ nandincpp/Merge Intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
+        vector<vector<int>> ans;
+        
+        if(!intervals.size())
+            return ans;
+        
+        sort(intervals.begin(), intervals.end()); //sorting
+        
+        vector<int> temp = intervals[0];
+        
+        for(auto i : intervals)
+        {
+            if(i[0] <= temp[1])
+                temp[1] = max(temp[1], i[1]);
+            
+            else
+            {
+                ans.push_back(temp);
+                temp = i;
+            }
+        }
+        
+        ans.push_back(temp);
+        return ans;
+    }
+};
