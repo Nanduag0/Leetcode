6f2dfa5bdcc1b6b1dd8336839860cf2972d97789
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2022-01-13 21:47:08 +0530

    Add files via upload

---
+++ nandincpp/Check if Every Row and Column Contains All Numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int n = matrix.length;
+        for (int r = 0; r < n; ++r) {
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for (int c = 0; c < n; ++c) {
+                row.add(matrix[r][c]);
+                col.add(matrix[c][r]);
+            }
+            if (row.size() < n || col.size() < n) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
