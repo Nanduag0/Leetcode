86b16e1cc856a09ab0bf777cd7a5071803b1cc36
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2022-01-13 21:44:34 +0530

    Add files via upload

---
+++ nandincpp/Search in a Binary Search Tree.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) 
+    {
+        return (root==NULL || root->val==val) ? root : ((val<root->val)?searchBST(root->left, val):searchBST(root->right, val));
+    }
+};
