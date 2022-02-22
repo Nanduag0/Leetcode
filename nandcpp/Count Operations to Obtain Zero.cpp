e2b66351e6bfa2f72ad03a4826ad88568de3490e
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2022-02-23 02:22:47 +0530

    Add files via upload

---
+++ nandcpp/Count Operations to Obtain Zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2)
+    {
+        int cnt = 0;
+        while(1)
+        {
+            if(num1==0 || num2==0)
+                break;
+            if(num1 >= num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+            
+            cnt++;
+        }
+        
+        return cnt;
+    }
+};
